972792ba835a91d2207b111e413a39e9464835ef
Alexander <ohleg@windowslive.com>
2017-11-29 23:27:59 +0100

    string concatenation for fewd challenge

---
+++ JAVASCRIPT/awesome-message.js
@@ -0,0 +1,5 @@
+var firstName = ('Alexander') ;
+var interest = ('cats');
+var hobby = ('play tennis');
+var awesomeMessage = 'Hi, my name is ' + firstName + '. I love ' + interest + '. In my spare time I like to ' + hobby + '.';
+console.log(awesomeMessage);
