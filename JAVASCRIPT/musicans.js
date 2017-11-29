a26e1526fd5c933a57fe7c6f27106cef05d6d147
Alexander <ohleg@windowslive.com>
2017-11-30 00:42:02 +0100

    named variable properly add 20 musicians var

--- JAVASCRIPT/musicans.js
+++ JAVASCRIPT/musicans.js
@@ -1,14 +1,17 @@
-var musicans = 1;
-if (musicans <= 0) {
+var musicians = 1;
+if (musicians <= 0) {
     console.log('not a group')
-} else if (musicans === 1) {
+} else if (musicians === 1) {
     console.log('solo');
-} else if (musicans === 2) {
+} else if (musicians === 2) {
     console.log('duet');
-} else if (musicans === 3) {
+} else if (musicians === 3) {
     console.log('trio');
-} else if (musicans === 4) {
-    console.log('duet');
-}else if (musicans > 4) {
+} else if (musicians === 4) {
+    console.log('quartet');
+}else if (musicians <20) {
     console.log('this is a larger group');
+}else if (musicians === 20) {
+    console.log('this is a large group');
+    
 }
