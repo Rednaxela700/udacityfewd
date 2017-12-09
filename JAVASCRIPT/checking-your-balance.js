2a02700119125f8508326532a75911e1fead6c9c
Alexander <ohleg@windowslive.com>
2017-12-09 22:46:14 +0100

    changed isActive variable to boolean

--- JAVASCRIPT/checking-your-balance.js
+++ JAVASCRIPT/checking-your-balance.js
@@ -1,17 +1,17 @@
-var checkBalance = (false);
-var balance = (100);
+var checkBalance = (true);
+var balance = (325.00);
 var isActive = (true);
 if (checkBalance === true) {
-    if (balance > 0 && isActive === "active") {
+    if (balance > 0 && isActive === true) {
         console.log("Your balance is $ " + balance.toFixed(2) + ".");
     }
-    else if (account !== "active") {
+    else if (account === false) {
         console.log("Your Account is no longer active");
     }
     else if (balance === 0) {
         console.log("Your Account is empty");
     }
-    else {
+    else if (balance < 0) {
         console.log("Your balance is negative. Please contact bank.");
     }
 }
