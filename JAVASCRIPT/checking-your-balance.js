eef2e8bae3abc1e9b405c1859f5c3113e8e0e4f9
Alexander <ohleg@windowslive.com>
2017-12-09 23:19:15 +0100

    fixed strings, now it works

--- JAVASCRIPT/checking-your-balance.js
+++ JAVASCRIPT/checking-your-balance.js
@@ -2,20 +2,19 @@ var balance = (325.00);
 var checkBalance = (true);
 var isActive = (true);
 
-if (checkBalance === true) {
+
     if (balance > 0 && isActive === true) {
-        console.log("Your balance is $ " + balance.toFixed(2) + ".");
+        console.log("Your balance is $" + balance.toFixed(2) + ".");
     }
     else if (checkBalance === true && isActive === false) {
-        console.log("Your Account is no longer active");
+        console.log("Your account is no longer active.");
     }
     else if (checkBalance === true && isActive === true && balance === 0) {
-        console.log("Your Account is empty");
+        console.log("Your account is empty.");
     }
     else if (checkBalance === true && isActive === true && balance < 0) {
         console.log("Your balance is negative. Please contact bank.");
     }
     else if (!checkBalance) {
-        console.log("Thank You. Have a nice day!")
+        console.log("Thank you. Have a nice day!");
     }
-}
