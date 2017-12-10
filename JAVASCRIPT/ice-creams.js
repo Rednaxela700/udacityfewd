d0230188ce2e0e3abddb17a3341a4e33f2bd0137
Alexander <ohleg@windowslive.com>
2017-12-10 09:34:36 +0100

    ice cream string concatenation

---
+++ JAVASCRIPT/ice-creams.js
@@ -0,0 +1,10 @@
+icecreams();
+
+function icecreams() {
+    var flavor = "strawberry";
+    var vessel = "cone";
+    var toppings = "cookies";
+    if (flavor === "strawberry" || "vanilla" && vessel === "cone" || bowl && toppings === "sprinkles" || "peanuts") {
+        console.log("I'd like two scoops of " + flavor + " ice cream in a " + vessel + "with " + toppings + ".");
+    }
+}
