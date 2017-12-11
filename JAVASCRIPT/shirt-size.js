b8e86dc04d0daed93412db034ac671bb1111e0ae
Alexander <ohleg@windowslive.com>
2017-12-11 01:25:09 +0100

    have to remove var size in time

---
+++ JAVASCRIPT/shirt-size.js
@@ -0,0 +1,8 @@
+var shirtWidth = 18; //Display size S variables
+var shirtLength = 28;
+var shirtSleeve = 8.13;
+var size = ("")
+if ((shirtWidth === 18) && (shirtLength === 28) && (shirtSleeve === 8.13)) {
+    (size = "S");
+    console.log("Your shirt size is: " + size + ".");
+}
