9e0d07d4ebd9c7044dd5a30126fc4963f6d0551c
Alexander <ohleg@windowslive.com>
2017-12-13 00:27:07 +0100

    logical expression with a ternary operator

---
+++ JAVASCRIPT/food-chain.js
@@ -0,0 +1,4 @@
+var eatsPlants = false;
+var eatsAnimals = true;
+var category = eatsPlants && eatsAnimals? ("omnivore") : (eatsAnimals? ("carnivore") : (eatsPlants? ("herbivore") : (undefined)));
+console.log(category);
